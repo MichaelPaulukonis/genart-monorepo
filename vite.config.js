982bbf2183e4c45f50cce69b30c12ac305f5a91c
MichaelPaulukonis <xraysmalevich@gmail.com>
2025-10-10 15:45:58 -0400

    fix: remove non-existent HTML files from vite config

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,7 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        single: resolve(__dirname, 'single_sketch.html'),
-        multi: resolve(__dirname, 'multi_sketch.html')
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
