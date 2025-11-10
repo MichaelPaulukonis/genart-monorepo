a3de66fe0616206e14550d21e7e511f90960d809
MichaelPaulukonis <xraysmalevich@gmail.com>
2025-11-10 08:52:01 -0500

    chore: release/deploy monochromifier, fix deploy script, and added a task

--- apps/monochromifier/src/utils/version-constants.js
+++ apps/monochromifier/src/utils/version-constants.js
@@ -1,3 +1,3 @@
 // This file is auto-generated during build - do not edit manually
-export const APP_VERSION = '0.3.0';
-export const BUILD_TIME = '2025-11-09T05:04:13.178Z';
+export const APP_VERSION = '0.4.0';
+export const BUILD_TIME = '2025-11-10T13:44:44.668Z';
