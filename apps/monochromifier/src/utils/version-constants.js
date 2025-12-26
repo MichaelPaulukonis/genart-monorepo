d2cf4530d5fb139530a498cbcf4d8862ee07b6fa
MichaelPaulukonis <xraysmalevich@gmail.com>
2025-12-26 14:22:16 -0500

    chore: updated version and task status

--- apps/monochromifier/src/utils/version-constants.js
+++ apps/monochromifier/src/utils/version-constants.js
@@ -1,3 +1,3 @@
 // This file is auto-generated during build - do not edit manually
 export const APP_VERSION = '0.5.0';
-export const BUILD_TIME = '2025-12-26T05:15:56.485Z';
+export const BUILD_TIME = '2025-12-26T16:46:54.280Z';
