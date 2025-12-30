2d9189babd31ec15c335b6be6db275dc48e69510
MichaelPaulukonis <xraysmalevich@gmail.com>
2025-12-30 12:46:20 -0500

    feat: fix deployment issues

--- apps/monochromifier/src/utils/version-constants.js
+++ apps/monochromifier/src/utils/version-constants.js
@@ -1,3 +1,3 @@
 // This file is auto-generated during build - do not edit manually
-export const APP_VERSION = '0.5.0';
-export const BUILD_TIME = '2025-12-26T19:26:16.624Z';
+export const APP_VERSION = '0.6.0';
+export const BUILD_TIME = '2025-12-30T17:40:27.759Z';
