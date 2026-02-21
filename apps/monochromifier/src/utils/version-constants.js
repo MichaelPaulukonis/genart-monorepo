22c822788785ee15815962faee40f8c50dacab46
Michael Paul Ukonis <michael@example.com>
2026-02-21 15:21:40 -0500

    chore(monochromifier): update build time and log specstory history

--- apps/monochromifier/src/utils/version-constants.js
+++ apps/monochromifier/src/utils/version-constants.js
@@ -1,3 +1,3 @@
 // This file is auto-generated during build - do not edit manually
 export const APP_VERSION = '0.6.0';
-export const BUILD_TIME = '2026-01-29T19:34:14.342Z';
+export const BUILD_TIME = '2026-02-21T14:54:58.332Z';
